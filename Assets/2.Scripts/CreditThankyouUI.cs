c6d5bc0228cbaa2405eb8f8d8cdb09638ad02483
GangHo <69287768+CBTvCardinal@users.noreply.github.com>
2023-11-24 23:27:47 +0900

    LHK
    20231124-1127 : _3 수정함.

--- Assets/2.Scripts/CreditThankyouUI.cs
+++ Assets/2.Scripts/CreditThankyouUI.cs
@@ -6,12 +6,14 @@ using UnityEngine.Video;
 public class CreditThankyouUI : MonoBehaviour
 {
     public VideoPlayer videoPlayer;
+    public SceneManager_ scene;
     public GameObject thankyou;
 
     // Start is called before the first frame update
     void Start()
     {
         videoPlayer.loopPointReached += OnVideoEnd;
+        thankyou.SetActive(false);
     }
 
     // Update is called once per frame
@@ -23,5 +25,10 @@ public class CreditThankyouUI : MonoBehaviour
     private void OnVideoEnd(VideoPlayer vp)
     {
         thankyou.SetActive(true);
+        Invoke("SceneChanger", 2f);
+    }
+
+    private void SceneChanger(){
+        scene.ToStartScene();
     }
 }
